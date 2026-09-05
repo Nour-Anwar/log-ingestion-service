25e500e4bfe4d4f2fe4b7e442c43bc41d116960a
Nour-Anwar <nooranwar067@gmail.com>
2026-09-05 22:20:41 +0300

    remove unused replica configuration

--- src/db/client.ts
+++ src/db/client.ts
@@ -2,8 +2,6 @@ import "dotenv/config";
 import postgres from "postgres";
 
 const primaryUrl = process.env.DATABASE_URL;
-const replicaUrl = process.env.READ_DATABASE_URL;
-
 if (!primaryUrl) {
   throw new Error("DATABASE_URL is not set");
 }
@@ -19,7 +17,7 @@ export const sql = postgres(primaryUrl, {
 });
 
 // Read queries
-export const readSql = postgres(replicaUrl ?? primaryUrl, {
+export const readSql = postgres(primaryUrl, {
   max: 15,
   idle_timeout: 20,
   connect_timeout: 5,
